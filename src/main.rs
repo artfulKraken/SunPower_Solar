b65915c18b5e477ca41d511d983d6d977e46afc6
artfulKraken <artfulKraken@proton.me>
2025-03-28 10:17:20 -0700

    v0.2.2 - panic crashing, I believe at consumption meter. Still trying to get it right with data_time

--- src/main.rs
+++ src/main.rs
@@ -948,12 +948,12 @@ fn get_sql_last_device_data( solar_sql_upload_conn: &mut PooledConn ) -> Result<
                                                     Some(cm) => {
                                                         print!("{:#?}", cm[0]);
                                                         print!("{:#?}", cm[1]);
-                                                        let dt = from_value::<chrono::NaiveDateTime>(cm[1].clone());
+                                                        //let dt = from_value::<chrono::NaiveDateTime>(cm[1].clone());
                                                         //let st = format!("{}", dt.format("%Y-%m-%d %H:%M:%S"));
                                                         //println!("TimeString: {}", st); 
                                                         consump_meter = ConsumptionMeter::set_values(
                                                             from_value::<String>(cm[0].clone()),
-                                                            NaiveDate::from_ymd_opt(2016, 7, 8).unwrap().and_hms_opt(9, 10, 11).unwrap(),
+                                                            from_value::<chrono::NaiveDateTime>(cm[1].clone()),
                                                             if cm[2] == "NULL".into() {None} else {Some( from_value::<f64>(cm[3].clone()) )}, 
                                                             if cm[3] == "NULL".into() {None} else {Some( from_value::<f64>(cm[3].clone()) )}, 
                                                             if cm[4] == "NULL".into() {None} else {Some( from_value::<f64>(cm[4].clone()) )},
@@ -976,8 +976,8 @@ fn get_sql_last_device_data( solar_sql_upload_conn: &mut PooledConn ) -> Result<
                                                         return Err( format!("No iconsumption meter matching device serial {} found.", s.0).into() )
                                                     },
                                                 }
-                                                println!("Made IT through, panicing now");
-                                                panic!("Good Stuff");
+                                                //println!("Made IT through, panicing now");
+                                                //panic!("Good Stuff");
                                             },
                                             Err(con_eff) => {
                                                 error!("{}", con_eff);
